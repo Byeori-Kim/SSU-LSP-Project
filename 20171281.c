923cd6f04d75bd984ee0d28d6612708868fce4b5
Byeori-Kim <99kbr11@gmail.com>
2021-05-26 02:19:15 -0700

    input file

--- 20171281.c
+++ 20171281.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) 
+{
+    FILE *file;
+    int **matrix;
+    file = fopen(argv[1], "r");
+    if(file == NULL)
+    {
+        fprintf(stderr, "fopen error for %s\n", argv[1]);
+        exit(1);
+    }
+    else
+    {
+        printf("Success!\nFilename: %s", argv[1]);
+
+        exit(0);
+    }
+
+
+
+}
