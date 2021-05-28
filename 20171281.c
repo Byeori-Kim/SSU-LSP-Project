82cfb2636757059ac5342968d64b1fac8df1e270
Byeori-Kim <99kbr11@gmail.com>
2021-05-28 07:55:31 -0700

    finish sequential mode

--- 20171281.c
+++ 20171281.c
@@ -2,21 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <time.h>
 
 #define Max_Int 20000
 
-void secq_mode(int **mat, int gen);
-void paral_mode(int **mat, int par_num, int gen);
-void thre_mode(int **mat, int par_num, int gen);
+int row;
+int col;
+int **matrix;
+
+int mat_check(int r, int c);
+void secq_mode(int gen);
+void secq_out();
+void paral_mode(int par_num, int gen);
+void paral_out(int par_num);
+void thre_mode(int par_num, int gen);
+void thre_out(int par_num);
 
 int main(int argc, char *argv[]) 
 {
     FILE *file;
-	int mode = 0;
-	int row;
-	int col;
-	int **matrix;
-   	char tmp;
+	int mode;
 
 	printf("(1)Process Termination (2)Sequential Processing (3)Parallel Processing (4)Multi-Thread Processing\nChoose mode: ");
 	scanf("%d", &mode);
@@ -24,6 +30,13 @@ int main(int argc, char *argv[])
 	while(mode != 1)
 	{
 		row = 1;
+		col = 1;
+		char *row_line;
+		char line[Max_Int];
+		char tmp;
+		clock_t start, end;
+		int result;
+		start = clock();
    		file = fopen(argv[1], "r");
    		if(file == NULL)
    		{
@@ -38,19 +51,50 @@ int main(int argc, char *argv[])
 		}
 		row = row+2;
 		matrix = (int**)malloc(sizeof(int*)*row);
-
-		printf("number of row is %d\n", row);
-	
+		fseek(file, 0, SEEK_SET);
+		row_line = fgets(line, Max_Int, file);
+		char *tok = strtok(row_line, " ");
+		while(tok != NULL)
+		{
+			col++;
+			tok = strtok(NULL, " ");
+		}
+		col++;
+		fseek(file, 0, SEEK_SET);
+		for(int i = 0; i < row; i++)
+		{
+			matrix[i] = (int*)calloc(col, sizeof(int));
+		}
+		for(int i = 1; i < row-1; i++)
+		{
+			row_line = fgets(line, Max_Int, file);
+			tok = strtok(row_line, " ");
+			for(int j = 1; j < col-1; j++)
+			{
+				matrix[i][j] = atoi(tok);
+				tok = strtok(NULL, " ");
+			}
+		}
+		/*for(int i = 0;i<row;i++){
+			for(int j=0;j<col;j++){
+				printf("%d ", matrix[i][j]);
+			}
+			printf("\n");
+		}*/
 
 		if(mode == 2)
 		{
 			int generation = 0;
 			printf("Put generation number: ");
 			scanf("%d", &generation);
-			for(int i = 0; i < generation; i++)
+			for(int i = 1; i < generation; i++)
 			{
-				secq_mode(matrix, i);
+				secq_mode(i);
 			}
+			secq_out();
+			end = clock();
+			result = (int)(end - start);
+			printf("Total elapsed time is %d ms.\n", result);
 		}
 		else if(mode == 3)
 		{
@@ -60,10 +104,14 @@ int main(int argc, char *argv[])
 			scanf("%d", &child);
 			printf("Put generation number: ");
 			scanf("%d", &generation);
-			for(int i = 0; i < generation; i++)
+			for(int i = 1; i < generation; i++)
 			{
-			paral_mode(matrix, child, i);
+			paral_mode(child, i);
 			}
+			paral_out(child);
+			end = clock();
+			result = (int)(end - start);
+			printf("Total elapsed time is %d ms.\n", result);
 		}
 		else if(mode == 4)
 		{
@@ -73,10 +121,14 @@ int main(int argc, char *argv[])
 			scanf("%d", &thread);
 			printf("Put generation number: ");
 			scanf("%d", &generation);
-			for(int i = 0; i < generation; i++)
+			for(int i = 1; i < generation; i++)
 			{
-			thre_mode(matrix, thread, i);
+			thre_mode(thread, i);
 			}
+			thre_out(thread);
+			end = clock();
+			result = (int)(end - start);
+			printf("Total elapsed time is %d ms.\n", result);
 		}
 		else
 		{
@@ -93,17 +145,134 @@ int main(int argc, char *argv[])
 
 }
 
-void secq_mode(int **mat, int gen)
+int mat_check(int r, int c)
 {
-	printf("In secq_mode function\n");
+	int count = 0;
+	if(matrix[r-1][c-1] == 1)
+		count++;
+	if(matrix[r-1][c] == 1)
+		count++;
+	if(matrix[r-1][c+1] == 1)
+		count++;
+	if(matrix[r][c-1] == 1)
+		count++;
+	if(matrix[r][c+1] == 1)
+		count++;
+	if(matrix[r+1][c-1] == 1)
+		count++;
+	if(matrix[r+1][c] == 1)
+		count++;
+	if(matrix[r+1][c+1] == 1)
+		count++;
+	return count;
 }
 
-void paral_mode(int **mat, int par_num, int gen)
+void secq_mode(int gen)
+{
+	char fname[20];
+	FILE *fp;
+	sprintf(fname, "gen_%d.matrix", gen);
+	fp = fopen(fname, "a");
+	int **mat;
+	mat = (int**)malloc(sizeof(int*)*row);
+	for(int i = 0; i < row; i++)
+	{
+		mat[i] = (int*)calloc(col, sizeof(int));
+	}
+	for(int i = 1; i < row-1; i++)
+	{
+		for(int j = 1; j < col-1; j++)
+		{
+			int count = mat_check(i,j);
+
+			if(matrix[i][j] == 0)
+			{
+				if(count == 4)
+					mat[i][j] = 1;
+				else
+					mat[i][j] = 0;
+				fprintf(fp, "%d ", mat[i][j]);
+			}
+			else if(matrix[i][j] == 1)
+			{
+				if(count<=2 || count>=7)
+					mat[i][j] = 0;
+				else
+					mat[i][j] = 1;
+				fprintf(fp, "%d ", mat[i][j]);
+			}
+		}
+		fprintf(fp, "\n");
+	}
+	for(int i = 1; i < row-1; i++)
+	{
+		for(int j = 1; j < col-1; j++)
+		{
+			matrix[i][j] = mat[i][j];
+		}
+	}
+}
+
+void secq_out()
+{
+	char fname[] = "output.matrix";
+	FILE *fp;
+	fp = fopen(fname, "a");
+	int **mat;
+	mat = (int**)malloc(sizeof(int*)*row);
+	for(int i = 0; i < row; i++)
+	{
+		mat[i] = (int*)calloc(col, sizeof(int));
+	}
+	for(int i = 1; i < row-1; i++)
+	{
+		for(int j = 1; j < col-1; j++)
+		{
+			int count = mat_check(i,j);
+
+			if(matrix[i][j] == 0)
+			{
+				if(count == 4)
+					mat[i][j] = 1;
+				else
+					mat[i][j] = 0;
+				fprintf(fp, "%d ", mat[i][j]);
+			}
+			else if(matrix[i][j] == 1)
+			{
+				if(count<=2 || count>=7)
+					mat[i][j] = 0;
+				else
+					mat[i][j] = 1;
+				fprintf(fp, "%d ", mat[i][j]);
+			}
+		}
+		fprintf(fp, "\n");
+	}
+	for(int i = 1; i < row-1; i++)
+	{
+		for(int j = 1; j < col-1; j++)
+		{
+			matrix[i][j] = mat[i][j];
+		}
+	}
+}
+void paral_mode( int par_num, int gen)
 {
 	printf("In paral_mode function\n");
 }
 
-void thre_mode(int **mat, int par_num, int gen)
+void paral_out( int par_num)
+{
+	printf("In paral_mode function\n");
+}
+
+void thre_mode(int par_num, int gen)
+{
+	printf("In thre_mode function\n");
+}
+
+void thre_out(int par_num)
 {
 	printf("In thre_mode function\n");
 }
